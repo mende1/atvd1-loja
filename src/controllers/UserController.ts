b5245ecb12f7bb3077961cf1fd8e1273c8dfc051
Gustavo Mendel <gustavo.mendel.br@gmail.com>
2022-01-25 12:09:33 -0300

    little changes

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,5 +1,5 @@
-import db from '../db';
-import { Request, Response } from 'express';
+// import { Request, Response } from 'express';
+
 
 class UserController {
   public id = 0;
@@ -11,27 +11,6 @@ class UserController {
   public updateId () {
     this.id = this.id + 1;
   }
-
-  public index (req: Request, res: Response) {
-    const getCommand = `
-      SELECT * FROM users;
-    `;
-    return db.query(getCommand, (err, result) => {
-      if (err) throw err;
-      res.end(JSON.stringify(result));
-    });
-  }
-
-  public post = (req: Request, res: Response) => {
-    const insertCommand = `
-    INSERT INTO users (user, password) VALUES ("${this.id}", "pw${this.id}")
-    `;
-    return db.query(insertCommand, (err, result) => {
-      if (err) throw err;
-      res.end(JSON.stringify(result));
-      this.updateId();
-    });
-  };
 }
 
 export default new UserController();
